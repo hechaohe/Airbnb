a432e65a131462904215a9eee1461292db7fdefb
hechaohe <550121308@qq.com>
2016-12-20 14:47:00 +0800

    Initial Commit

---
+++ Airbnb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Airbnb
+//
+//  Created by hc on 2016/12/20.
+//  Copyright © 2016年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
